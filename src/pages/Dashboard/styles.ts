d0cb1d1fdf055571ee6b77c6d88292e705949798
Marthin Korb <marthinkorb@gmail.com>
2020-07-01 00:03:37 -0300

    Building Main Page

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,10 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { shade } from 'polished';
+
+interface FormProps{
+  hasError: boolean;
+}
 
 export const Title = styled.h1`
   font-size: 48px;
@@ -11,7 +15,7 @@ export const Title = styled.h1`
   margin-top: 80px;
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   margin-top: 40px;
   max-width: 700px;
 
@@ -24,6 +28,12 @@ export const Form = styled.form`
     border: 0;
     border-radius: 5px 0 0 5px;
     color: #3a3a3a;
+    border: 1px solid #fff;
+    border-right: 0;
+
+    ${(props) => props.hasError && css`
+      border-color: #c53030;
+    `}
 
     &::placeholder {
       color: #a8a8b3;
@@ -43,6 +53,12 @@ export const Form = styled.form`
       background: ${shade(0.2, '#04d361')};
     }
   }
+`;
+
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin: 8px 5px;
 `;
 
 export const Repositories = styled.div`
@@ -76,7 +92,8 @@ export const Repositories = styled.div`
     }
 
     div {
-      margin-left: 16px;
+      margin: 0 16px;
+      flex: 1;
 
       strong {
         font-size: 20px;
@@ -96,3 +113,5 @@ export const Repositories = styled.div`
     }
   }
 `;
+
+
